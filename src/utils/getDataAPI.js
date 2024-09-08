15c050417fc2ee873e825be3a1aaed0764e95f46
Maxim93mk <kinjashov.m@rambler.ru>
2024-09-08 18:18:40 +0300

    Установка библиотеки axios

--- src/utils/getDataAPI.js
+++ src/utils/getDataAPI.js
@@ -1,1 +1,11 @@
-const apiid = '35c6ba00b7d1a23174ed6f5acca058c6';
+import { useState } from "react";
+
+
+const useGetWeatherData = ()=>{ 
+    const api = 'https://api.openweathermap.org/data/2.5/weather?';
+
+    const apiid = '35c6ba00b7d1a23174ed6f5acca058c6';
+
+    // https://api.openweathermap.org/data/2.5/weather?q=Париж,RU&units=metric&appid=35c6ba00b7d1a23174ed6f5acca058c6&lang=ru
+
+}
