f169b82c7d0c356fc6ee647a3147dbfa3793c760
Maxim93mk <kinjashov.m@rambler.ru>
2024-09-14 19:34:42 +0300

    Прокинуты погодные условия

--- src/components/weather-card/WeatherCard.jsx
+++ src/components/weather-card/WeatherCard.jsx
@@ -5,16 +5,23 @@ import SunBlock from '../sun-block/SunBlock';
 
 function WeatherCard(props) {
 
+  const customRound=(value, decimals)=> {
+    return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
+  }
+
   return (
     <>
       <div className={styles.card}>
-        <TempBlock 
-                   main={props.main}
-                   sys={props.sys}
-                   imgURL={props.imgURL} />
-        <WeatherConditionsBlock wind={props.wind}/>
+        <TempBlock
+          main={props.main}
+          sys={props.sys}
+          imgURL={props.imgURL} />
+        <WeatherConditionsBlock wind={props.wind}
+          humidity={props.main.humidity}
+          pressure={props.main.pressure} 
+          customRound={customRound}/>
         <SunBlock sunrise={props.sys.sunrise}
-                  sunset={props.sys.sunset}/>
+          sunset={props.sys.sunset} />
       </div>
     </>
   );
--- src/components/weather-conditions-block/WeatherConditionsBlock.jsx
+++ src/components/weather-conditions-block/WeatherConditionsBlock.jsx
@@ -1,25 +1,28 @@
 import styles from './WeatherConditionsBlock.module.css';
 
-function WeatherConditionsBlock() {
+function WeatherConditionsBlock(props) {
+
+
+  const pressure = props.customRound(props.pressure*0.75, 2);
 
   return (
     <>
-      <div className={styles.main}>
+      <div className={styles.block}>
         <div className={styles.wind}>
-          <img src="assets/img/windsock.svg" alt="" />
-          <p className='descrText'>4.29 м/с</p>
+          <img src="assets/img/windsock.svg" alt="Скорость ветра" />
+          <p className='descrText'>{props.wind.speed} м/с</p>
         </div>
         <div className={styles.wind}>
-          <img src="assets/img/compass.svg" alt="" />
+          <img src="assets/img/compass.svg" alt="Направление ветра" />
           <p className='descrText'>ЮЗ</p>
         </div>
         <div className={styles.wind}>
-          <img src="assets/img/humidity.svg" alt="" />
-          <p className='descrText'>100%</p>
+          <img src="assets/img/humidity.svg" alt="Влажность" />
+          <p className='descrText'>{props.humidity} %</p>
         </div>
         <div className={styles.wind}>
-          <img src="assets/img/barometer.svg" alt="" />
-          <p className='descrText'>1000 мм рт. ст.</p>
+          <img src="assets/img/barometer.svg" alt="Барометр" />
+          <p className='descrText'>{pressure} мм рт. ст.</p>
         </div>
       </div>
 
